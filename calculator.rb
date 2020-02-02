b6d30c9ee7a284f6cd786639946706f0813c8e43
Daniel Loyd <dhloyd@icloud.com>
2020-02-02 06:34:19 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,8 +1,9 @@
 first_number = 4.0
 second_number = 3.0
 
-def sum(first_number, second_number)
-  sum = first_number + second_number
-end 
+sum = first_number + second_number
+# def addition(first_number, second_number)
+#   sum = first_number + second_number
+# end 
 
 #sum(first_number, second_number)
